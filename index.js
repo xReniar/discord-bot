e8b3c60ce059f5a11224e582a8ca5e2d74f4185c
xReniar <ilaorainer@gmail.com>
2024-03-04 23:04:24 +0100

    feat: server start after bot logs in

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+/* importing necessary modules */
 const express = require("express")
 const app = express()
+const { Events } = require("discord.js")
 
-const { server } = require("./conf.json")
+/* conf.json, server routing and discord event handler */
+const { discord, server } = require("./conf.json")
 const routes = require("./src/controller")
+const client = require("./src/discordEventHandler")
 
 // Mount the routes from controller.js
 app.use("/", routes);
 
-app.listen(server.port,() => {
-    console.log(`Listening on port ${server.port}`)
+/* once client is ready start the server */
+client.once(Events.ClientReady, (client) => {
+    console.log(`Logged in as ${client.user.tag}`)
+    app.listen(server.port,() => {
+        console.log(`Listening on port ${server.port}`)
+    })
 })
+
+client.login(discord.token)
