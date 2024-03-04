6db745b272b36bc34e4ca0e609ccb325c9b676be
xReniar <ilaorainer@gmail.com>
2024-03-04 23:14:04 +0100

    fix: now server start only when bot is ready

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /* importing necessary modules */
 const express = require("express")
 const app = express()
-const { Events } = require("discord.js")
 
 /* conf.json, server routing and discord event handler */
 const { discord, server } = require("./conf.json")
@@ -12,11 +11,9 @@ const client = require("./src/discordEventHandler")
 app.use("/", routes);
 
 /* once client is ready start the server */
-client.once(Events.ClientReady, (client) => {
-    console.log(`Logged in as ${client.user.tag}`)
-    app.listen(server.port,() => {
-        console.log(`Listening on port ${server.port}`)
-    })
-})
-
 client.login(discord.token)
+    .then((value) => {
+        app.listen(server.port,() => {
+            console.log(`Listening on port ${server.port}`)
+        })
+    })
