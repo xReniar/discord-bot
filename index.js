e802852829ec0dcd3289c36dd83794147472dbd2
xReniar <ilaorainer@gmail.com>
2024-03-04 22:03:08 +0100

    feat: added index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("ciao");
+const express = require("express")
+const app = express()
+const { server } = require("./conf.json")
+
+app.get("/",(req,res) => {
+    res.send("Hello World")
+})
+
+app.listen(server.port,() => {
+    console.log(`Listening on port ${server.port}`)
+})
