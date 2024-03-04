911bc30402062743e7d9e79b93b06ecc9bb36130
xReniar <ilaorainer@gmail.com>
2024-03-04 22:22:26 +0100

    feat: importing routing from contorller.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const app = express()
-const { server } = require("./conf.json")
 
-app.get("/",(req,res) => {
-    res.send("Hello World")
-})
+const { server } = require("./conf.json")
+const routes = require("./src/controller")
+
+// Mount the routes from controller.js
+app.use("/", routes);
 
 app.listen(server.port,() => {
     console.log(`Listening on port ${server.port}`)
