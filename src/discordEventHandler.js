219f274ad5e4b82ccda659c8692a7dd6b501d23b
xReniar <ilaorainer@gmail.com>
2024-03-06 22:03:45 +0100

    feat: added MessageContent intent

--- src/discordEventHandler.js
+++ src/discordEventHandler.js
@@ -4,7 +4,8 @@ const { Client, Events, GatewayIntentBits } = require("discord.js")
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
 
