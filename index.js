637abec816b02a4ada16b68c732c5ba47483eea2
xReniar <ilaorainer@gmail.com>
2024-03-10 22:56:08 +0100

    feat: Client is now defined inside index.js

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 /* importing necessary modules */
 const express = require("express")
 const app = express()
+const { Client, GatewayIntentBits } = require("discord.js")
 
-/* conf.json, server routing and discord event handler */
+/* conf.json, server routing and discord various handlers */
 const { discord, server } = require("./conf.json")
 const routes = require("./src/controller")
-const client = require("./src/discordEventHandler")
+const discordEventHandler = require("./src/discord/eventhandler")
+const discordCommandHandler = require("./src/discord/commandhandler")
 
 // Mount the routes from controller.js
 app.use("/", routes);
 
+// initializing client with necessary intents
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+// discord client setup
+discordEventHandler(client)
+discordCommandHandler(client)
+
 /* once client is ready start the server */
 client.login(discord.token)
-    .then((value) => {
+    .then(() => {
         app.listen(server.port,() => {
             console.log(`Listening on port ${server.port}`)
         })
