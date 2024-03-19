fca667866273e052d6435e45fd815aed9d7b70c7
xReniar <ilaorainer@gmail.com>
2024-03-19 11:01:52 +0100

    feat: added interaction event handler for slash commands

--- src/discord/eventhandler.js
+++ src/discord/eventhandler.js
@@ -8,9 +8,31 @@ function discordEventHandler(client) {
     client.once(Events.ClientReady, (client) => {
         console.log(`Logged in as ${client.user.tag}`)
     })
+
+    client.on(Events.InteractionCreate, async (interaction) => {
+        // only accepting slash commands
+        if(!interaction.isChatInputCommand()) return;
+
+        const command = interaction.client.commands.get(interaction.commandName);
+        if(!command) return;
+
+        try {
+            await command.execute(interaction);
+        } catch(error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+            } else {
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+        }
+    })
     
     client.on(Events.MessageCreate,(message) => {
         console.log(`${message.author.username}: ${message.content}`)
+        if(message.content == "say hi"){
+            message.reply("Hi!")
+        }
     })
 }
 
