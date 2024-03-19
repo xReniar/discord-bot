603b95f2b9d106b82326b1fefbed8049712326bc
xReniar <ilaorainer@gmail.com>
2024-03-19 10:50:09 +0100

    feat: added function to register commands

--- src/discord/commandhandler.js
+++ src/discord/commandhandler.js
@@ -1,8 +1,10 @@
-const { Collection } = require("discord.js")
+const { Collection, REST, Routes } = require("discord.js")
+const { discord } = require("../../conf.json")
 const path = require("node:path")
 const fs = require("node:fs")
 
 function importSlashCommands(client){
+    const commands = []
     client.commands = new Collection();
 
     const foldersPath = path.join(__dirname,"..","..","commands")
@@ -18,15 +20,34 @@ function importSlashCommands(client){
             
             if ('data' in command && 'execute' in command) {
                 client.commands.set(command.data.name, command);
+                commands.push(command.data.toJSON())
             } else {
                 console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
             }
         }
     }
+    return commands
+}
+
+function registerSlashCommands(commands){
+    const rest = new REST().setToken(discord.token);
+    (async () => {
+        try {
+            console.log(`Started refreshing ${commands.length} application (/) commands`)
+            const data = await rest.put(
+                Routes.applicationGuildCommands(discord.client_id,discord.guild_id),
+                { body: commands },
+            )
+            console.log(`Succesfully reloaded ${data.length} application (/) commands`)
+        } catch(error) {
+            console.error(error)
+        }
+    })();
 }
 
 function discordCommandHandler(client){
-    importSlashCommands(client)
+    commands = importSlashCommands(client)
+    registerSlashCommands(commands)
 }
 
 module.exports = discordCommandHandler
