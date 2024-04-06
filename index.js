b0f8ae5a50c0c54227b3af4a861a41fd010ee6c4
xReniar <ilaorainer@gmail.com>
2024-04-07 00:52:26 +0200

    feat: removed client from index, just importing from setup.js

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 /* importing necessary modules */
 const express = require("express")
 const app = express()
-const { Client, GatewayIntentBits } = require("discord.js")
+const client = require("./src/discord/setup")
 
 /* conf.json, server routing and discord various handlers */
 const { discord, server } = require("./conf.json")
 const routes = require("./src/controller")
-const discordEventHandler = require("./src/discord/eventhandler")
-const discordCommandHandler = require("./src/discord/commandhandler")
 
 // Mount the routes from controller.js
 app.use("/", routes);
-
-// initializing client with necessary intents
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildIntegrations
-    ]
-})
-
-// discord client setup
-discordEventHandler(client)
-discordCommandHandler(client)
 
 /* once client is ready start the server */
 client.login(discord.token)
